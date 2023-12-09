f48f93ccdde6fdd11e03ab924972ce9c25ac52e4
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-12-09 17:38:53 -0300

    style: rename the http module to "chttp"

--- tests/test_chttp_compareHttpRequests.c
+++ tests/test_chttp_compareHttpRequests.c
@@ -1,43 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
-#include "cp_src/http_parser/request.h"
-#include "cp_src/http_parser/method.h"
-#include "cp_src/http_parser/route.h"
-#include "cp_src/http_parser/version.h"
-#include "cp_src/http_parser/field.h"
+#include "cp_src/chttp/request.h"
+#include "cp_src/chttp/method.h"
+#include "cp_src/chttp/route.h"
+#include "cp_src/chttp/version.h"
+#include "cp_src/chttp/field.h"
 
 // Criação da variável de teste usando as structs fornecidas
 HttpRequest_t referenceRequest;
-
-// void test_http_parser_happy_path()
-// {
-//     char *given = "GET / HTTP/1.1\r\n"
-//                   "Host: your-api-endpoint\r\n"
-//                   "Connection: close\r\n"
-//                   "\r\n";
-
-//     // Criação da variável de teste usando as structs fornecidas
-//     HttpRequest_t expect;
-
-//     // Preenchimento da variável de teste com os dados fornecidos
-//     expect.method.value = "GET";
-//     expect.route.value = "/";
-//     expect.version.value = "HTTP/1.1";
-
-//     // Preenchimento dos campos da struct Field (Host e Connection)
-//     expect.fields[0].key = "Host";
-//     expect.fields[0].value = "your-api-endpoint";
-//     expect.fields[1].key = "Connection";
-//     expect.fields[1].value = "close";
-
-//     // Corpo da requisição (vazio no exemplo)
-//     expect.body = "";
-    
-//     HttpRequest_t result;
-//     http_parser(&given, &result);
-    
-//     assert(compareHttpRequest(&expect, &result));
-// }
 
 char* test_http_compare_happy_path()
 {
@@ -76,6 +46,6 @@ int main()
 
     printf("test_http_compare_happy_path: %s\n", test_http_compare_happy_path());
     printf("test_http_compare_not_equal: %s\n", test_http_compare_not_equal());
-    // test_http_parser_happy_path();
+
     return 0;
 }
