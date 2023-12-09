6081d9e8e1e112cd09f75ceea173963f5b20349d
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-12-09 03:36:01 -0300

    chore: compile each test file and run it

--- test.sh
+++ test.sh
@@ -1,10 +1,28 @@
 #/bin/bash;
+test_folder=tests
+cp_src="$test_folder/cp_src/"
+output_ext=".test.out"
 
 # copy the source file to the test folder
-cp -rf src/* tests/cp_src/
+cp -rf src/* "$cp_src"
+
+# list get all file of tests
+c_files=$(find "$test_folder" -type f -name "*.c" -not -path "*/cp_src/*")
 
 # compile the each test
-gcc tests/test_*.c -o tests/test_*.c.test.out
+for file in "$c_files"; do
+    echo
+    echo "Compiling $file file!"
+    gcc $file tests/cp_src/*.c  -o "$file$output_ext"
+    echo "................."
+    echo
+done
 
 # run each test
-./tests/test_*.c.test.out
+for file in "$c_files"; do
+    echo "Running the $file test!"
+    $file$output_ext
+    echo 
+    echo "................."
+    echo
+done
